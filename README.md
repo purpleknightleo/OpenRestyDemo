d00e901e478a34051c7a96d844f39986efe9c22c
lifan <lifan@u51.com>
2017-10-10 20:26:06 +0800

    [add]load balance

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 This project is a demo for OpenResty
 
-1. use require to import custom lib: curl 'localhost:8080/add?a=8&b=3'    
-2. local cache: curl 'localhost:8080/cache?key=fuck'    
-3. redis: curl 'localhost:8080/redis?cmd=get&key=a'      
-4. mysql: curl 'localhost:8080/mysql'      
+1. load-balance: curl localhost:8080   
+2. use require to import custom lib: curl 'localhost:8080/add?a=8&b=3'    
+3. local cache: curl 'localhost:8080/cache?id=fuck'    
+4. redis: curl 'localhost:8080/redis?cmd=get&key=a'      
+5. mysql: curl 'localhost:8080/mysql'      
 
    
 
--- lua/cache.lua
+++ lua/cache.lua
@@ -2,13 +2,13 @@
 -- ref: http://www.cnblogs.com/digdeep/p/4859575.html
 
 function set_to_cache(key, val, exp)
-	local ngx_cache = ngx.shared.ngx_cache
+	local ngx_cache = ngx.shared.user_cache   -- this cache is defined in nginx.conf
 	local succ, err, forcible ngx_cache:set(key, val, exp)
 	return succ
 end
 
 function get_from_cache(key)
-	local ngx_cache = ngx.shared.ngx_cache
+	local ngx_cache = ngx.shared.user_cache
 	local val = ngx_cache:get(key)
 	if not val then
 		val = 'dummy'
@@ -17,6 +17,6 @@ function get_from_cache(key)
 	return val
 end
 
-local key = tostring(ngx.var.arg_key)
-local val = get_from_cache(key)
+local id = tostring(ngx.var.arg_id)
+local val = get_from_cache(id)
 ngx.say(val)
